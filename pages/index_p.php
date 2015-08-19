273acb55cbced38621de7f2770c0149acfff0c93
Johan Morales <jhellmetal2000@gmail.com>
2015-08-18 22:49:56 -0500

    creación de página index_p.php con sus partes dinamicas.

---
+++ pages/index_p.php
@@ -0,0 +1,25 @@
+<?php 
+
+	include "encabezado.php";
+	include "menu_superior.php";
+	include "menu_lateral.php";
+
+ ?>
+	
+	<div id="page-wrapper">
+		
+			<div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Contenido de la pagina</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+	
+	
+	</div>
+
+ <?php 
+
+ 	include "footer.php";
+
+  ?>
