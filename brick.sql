b389945f1d4d00a9155dbd589701a121b443630b
Johan Morales <jhellmetal2000@gmail.com>
2015-09-01 12:10:19 -0500

    Nueva version de la base de datos en la entidad materiales y derivados según la versión 2 del modelo entidad relacion.

--- brick.sql
+++ brick.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.12deb2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 31-08-2015 a las 21:57:14
--- Versión del servidor: 5.5.44-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.11
+-- Tiempo de generación: 01-09-2015 a las 12:08:33
+-- Versión del servidor: 5.6.25-0ubuntu0.15.04.1
+-- Versión de PHP: 5.6.4-4ubuntu6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,14 +23,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `material`
+--
+
+CREATE TABLE IF NOT EXISTS `material` (
+`pkID` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `marca` varchar(200) NOT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `fkID_clase` int(11) DEFAULT NULL,
+  `fkID_tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `material`
+--
+
+INSERT INTO `material` (`pkID`, `nombre`, `precio`, `marca`, `imagen`, `fkID_clase`, `fkID_tipo`) VALUES
+(1, 'Bloque naranja hueco', 2500, 'Santafé', 'Bloque.png', NULL, NULL),
+(2, 'Ladrillo Común', 3200, 'Ladrillo-S.A.D', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `material_propiedad`
+--
+
+CREATE TABLE IF NOT EXISTS `material_propiedad` (
+  `fkID_material` int(11) NOT NULL,
+  `fkID_propiedad` int(11) NOT NULL,
+  `valor` varchar(200) NOT NULL,
+  `fkID_uMedida` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `material_propiedad`
+--
+
+INSERT INTO `material_propiedad` (`fkID_material`, `fkID_propiedad`, `valor`, `fkID_uMedida`) VALUES
+(1, 1, '2.4', 1),
+(1, 2, '3.6', 1),
+(1, 3, '6.4', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `propiedad`
+--
+
+CREATE TABLE IF NOT EXISTS `propiedad` (
+`pkID` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `propiedad`
+--
+
+INSERT INTO `propiedad` (`pkID`, `nombre`) VALUES
+(1, 'ancho'),
+(2, 'alto'),
+(3, 'largo');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tipo_usuario`
 --
 
 CREATE TABLE IF NOT EXISTS `tipo_usuario` (
-  `pkID` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  PRIMARY KEY (`pkID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+`pkID` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tipo_usuario`
@@ -47,15 +112,14 @@ INSERT INTO `tipo_usuario` (`pkID`, `nombre`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `pkID` int(11) NOT NULL AUTO_INCREMENT,
+`pkID` int(11) NOT NULL,
   `alias` varchar(50) NOT NULL,
   `pass` varchar(50) NOT NULL,
   `nombres` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `numero_cc` int(11) NOT NULL,
-  `fkID_tipo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`pkID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `fkID_tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -64,6 +128,89 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 INSERT INTO `usuarios` (`pkID`, `alias`, `pass`, `nombres`, `apellidos`, `numero_cc`, `fkID_tipo`) VALUES
 (1, 'jsmorales', '8cb2237d0679ca88db6464eac60da96345513964', 'Johan', 'Morales', 1024524163, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `u_medida`
+--
+
+CREATE TABLE IF NOT EXISTS `u_medida` (
+`pkID` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `abreviatura` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `u_medida`
+--
+
+INSERT INTO `u_medida` (`pkID`, `nombre`, `abreviatura`) VALUES
+(1, 'centímetro', 'cm'),
+(2, 'metro', 'm');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `material`
+--
+ALTER TABLE `material`
+ ADD PRIMARY KEY (`pkID`);
+
+--
+-- Indices de la tabla `propiedad`
+--
+ALTER TABLE `propiedad`
+ ADD PRIMARY KEY (`pkID`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+ ADD PRIMARY KEY (`pkID`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+ ADD PRIMARY KEY (`pkID`);
+
+--
+-- Indices de la tabla `u_medida`
+--
+ALTER TABLE `u_medida`
+ ADD PRIMARY KEY (`pkID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `material`
+--
+ALTER TABLE `material`
+MODIFY `pkID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `propiedad`
+--
+ALTER TABLE `propiedad`
+MODIFY `pkID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+MODIFY `pkID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+MODIFY `pkID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `u_medida`
+--
+ALTER TABLE `u_medida`
+MODIFY `pkID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
